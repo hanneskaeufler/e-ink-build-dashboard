e9819bc8a27fb35d393844fca89aec4e5fbc912e
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-02-11 00:51:48 +0100

    Extract tiny functions

--- main.py
+++ main.py
@@ -5,16 +5,28 @@ from fetch_build_status import fetch_build_status
 import os
 
 
+def fetch_bitrise_build_status(app, token):
+    return fetch_build_status(
+        'https://www.bitrise.io/app/' + app + '.svg?token=' + token)
+
+def fetch_travis_build_status(repo):
+    return fetch_build_status(
+        'https://travis-ci.org/hanneskaeufler/'+ repo + '.svg?branch=master')
+
+def fetch_circle_build_status(repo):
+    return fetch_build_status(
+        'https://circleci.com/gh/hanneskaeufler/' + repo '.svg?style=svg')
+
 def main():
     epd = epd7in5.EPD()
     epd.init()
 
     projects = [
-        ('PitBuddy iOS', fetch_build_status('https://www.bitrise.io/app/' + os.environ['PB_IOS_APP'] + '.svg?token=' + os.environ['PB_IOS_TOKEN'])),  # noqa
-        ('PitBuddy Android', fetch_build_status('https://www.bitrise.io/app/' + os.environ['PB_ANDROID_APP'] + '.svg?token=' + os.environ['PB_ANDROID_TOKEN'])),  # noqa
-        ('Blog', fetch_build_status('https://circleci.com/gh/hanneskaeufler/blog.svg?style=svg')),  # noqa
-        ('danger-todoist', fetch_build_status('https://travis-ci.org/hanneskaeufler/danger-todoist.svg?branch=master')),  # noqa
-        ('danger-plugin-mentor', fetch_build_status('https://travis-ci.org/hanneskaeufler/danger-plugin-mentor.svg?branch=master'))  # noqa
+        ('PitBuddy iOS', fetch_bitrise_build_status(os.environ['PB_IOS_APP'], os.environ['PB_IOS_TOKEN'])),  # noqa
+        ('PitBuddy Android', fetch_bitrise_build_status(os.environ['PB_ANDROID_APP'], os.environ['PB_ANDROID_TOKEN'])),  # noqa
+        ('Blog', fetch_circle_build_status('blog')),
+        ('danger-todoist', fetch_travis_build_status('danger-todoist')),
+        ('danger-plugin-mentor', fetch_travis_build_status('danger-plugin-mentor'))  # noqa
     ]
 
     image = (
