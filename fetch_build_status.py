a1524dffb7cdcb399cae753d5b3a09d18484b46f
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-02-10 22:27:40 +0100

    Circle CI has 'PASSED' in the svg

--- fetch_build_status.py
+++ fetch_build_status.py
@@ -4,7 +4,7 @@ import dash
 def fetch_build_status(url):
     svg = urllib2.urlopen(url).read()
 
-    if 'passing' in svg:
+    if 'pass' in svg.lower():
         return dash.BuildStatus.passed
 
     return dash.BuildStatus.failed
