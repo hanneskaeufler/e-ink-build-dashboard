658fbd99f35d963df67c59ad8222212073bb2c30
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-02-10 21:40:51 +0100

    Fetch build status for current projects

--- main.py
+++ main.py
@@ -32,17 +32,20 @@ import datetime
 #import imagedata
 import codecs
 import dash
+import fetch_build_status
+import os
 
 def main():
     epd = epd7in5.EPD()
     epd.init()
 
     projects = [
-        ('PitBuddy iOS', dash.BuildStatus.passed),
-        ('PitBuddy Android', dash.BuildStatus.passed),
-        ('Blog', dash.BuildStatus.failed),
-        ('danger-todoist', dash.BuildStatus.passed),
-        ('danger-plugin-mentor', dash.BuildStatus.passed)]
+        ('PitBuddy iOS', fetch_build_status('https://www.bitrise.io/app/' + os.environ['PB_IOS_APP'] + '.svg?token=' + os.environ['PB_IOS_TOKEN'])),
+        ('PitBuddy Android', fetch_build_status('https://www.bitrise.io/app/' + os.environ['PB_ANDROID_APP'] + '.svg?token=' + os.environ['PB_ANDROID_TOKEN'])),
+        ('Blog', fetch_build_status('https://circleci.com/gh/hanneskaeufler/blog.svg?style=svg')),
+        ('danger-todoist', fetch_build_status('https://travis-ci.org/hanneskaeufler/danger-todoist.svg?branch=master')),
+        ('danger-plugin-mentor', fetch_build_status('https://travis-ci.org/hanneskaeufler/danger-plugin-mentor.svg?branch=master'))
+    ]
 
     image = dash.Dash(projects, '/usr/share/fonts/truetype/lato/Lato-Regular.ttf').render()
 
