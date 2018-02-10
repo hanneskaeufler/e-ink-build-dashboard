618dc7f3e22e5edfdc1c6d854c58218ff1b43c83
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-02-10 18:01:51 +0100

    Sample black rect in upper left corner

--- dash.py
+++ dash.py
@@ -1,11 +1,19 @@
 from PIL import Image
+from PIL import ImageDraw
 
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
 
 class Dash:
     def __init__(self):
-        self.image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
+        self.image = self.__clear_image()
+        self.draw = ImageDraw.Draw(self.image)
 
     def render(self):
+        self.draw.rectangle((0, 0, 10, 10), fill = 0)
+
         return self.image
+
+    def __clear_image(self):
+        return Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
+
