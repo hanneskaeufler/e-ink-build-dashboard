9f20abc3db43b191a2667a193472e2c0613eaba7
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-06-29 18:36:05 +0200

    Fix build status detection

--- fetch_build_status.py
+++ fetch_build_status.py
@@ -6,7 +6,7 @@ import urllib2
 def fetch_build_status(url):
     svg_or_json = urllib2.urlopen(url).read()
 
-    if re.compile('pass|success').match(svg_or_json.lower()):
+    if re.search('pass|success', svg_or_json.lower()):
         return dash.BuildStatus.passed
 
     return dash.BuildStatus.failed
